6b668e19925e36d6582f1ed8ddfd72332bee491a
Mutton <torpedoente@googlemail.com>
2015-01-22 14:26:49 +0100

    small correction of Doxygen documentation
    (cherry picked from commit cb7b5b141305c9c504ee244f0fed10ca8893fddd)
    
    Conflicts:
    	GMP_Server/Network/Server.cs

--- GMP_Server/Network/Server.cs
+++ GMP_Server/Network/Server.cs
@@ -27,9 +27,14 @@ namespace GUC.Server.Network
         public BitStream SendBitStream { get; private set; }
 
 
+        /** 
+        * Server-class which defines and manages network transfer as well as the game loop.
+        * It uses RakNet network messages to establish communication channels between server and clients.
+        * It further counts the connections and initialiszes the overall game loop.
+        */
         public Server()
         {
-            ServerInterface = RakPeer.GetInstance();
+            ServerInterface = RakPeer.GetInstance(); /**< Instance of RakNets main interface for network communication. */
             MessageListener = new Dictionary<byte,IMessage>();
             ReceiveBitStream = new BitStream();
             SendBitStream = new BitStream();
@@ -38,45 +43,52 @@ namespace GUC.Server.Network
             initMessageListener();
         }
 
-        protected void initMessageListener()
-        {
-            MessageListener.Add((byte)NetworkID.ConnectionMessage, new ConnectionMessage());
-            MessageListener.Add((byte)NetworkID.NPCSpawnMessage, new NPCSpawnMessage());
-            MessageListener.Add((byte)NetworkID.GuiMessage, new GuiMessage());
-            MessageListener.Add((byte)NetworkID.OnDamageMessage, new OnDamageMessage());
+		protected void initMessageListener()
+		{
+			MessageListener.Add((byte)NetworkID.ConnectionMessage, new ConnectionMessage()); /**< Network messages concerning player connection state. */
+			MessageListener.Add((byte)NetworkID.NPCSpawnMessage, new NPCSpawnMessage()); /**< Network messages concerning spawning npcs. */
+			MessageListener.Add((byte)NetworkID.GuiMessage, new GuiMessage()); /**< Network messages concerning GUI events. */
+			MessageListener.Add((byte)NetworkID.OnDamageMessage, new OnDamageMessage()); /**< Network messages concerning damage events. */
 
-            MessageListener.Add((byte)NetworkID.TakeItemMessage, new TakeItemMessage());
-            MessageListener.Add((byte)NetworkID.DropItemMessage, new DropItemMessage());
+			MessageListener.Add((byte)NetworkID.TakeItemMessage, new TakeItemMessage()); /**< Network messages concerning events which are triggered when world-items are taken. */
+			MessageListener.Add((byte)NetworkID.DropItemMessage, new DropItemMessage()); /**< Network messages concerning events which are triggered when items are dropped into the world. */
 
-            MessageListener.Add((byte)NetworkID.SetVobPosDirMessage, new SetVobPosDirMessage());
-            MessageListener.Add((byte)NetworkID.AnimationUpdateMessage, new AnimationUpdateMessage());
+			MessageListener.Add((byte)NetworkID.SetVobPosDirMessage, new SetVobPosDirMessage()); /**< Network messages concerning forced vob positioning (in world). */
+			MessageListener.Add((byte)NetworkID.AnimationUpdateMessage, new AnimationUpdateMessage()); /**< Network messages concerning npc animations. */
 
-            MessageListener.Add((byte)NetworkID.NPCUpdateMessage, new NPCUpdateMessage());
-            MessageListener.Add((byte)NetworkID.MobInterMessage, new MobInterMessage());
+			MessageListener.Add((byte)NetworkID.NPCUpdateMessage, new NPCUpdateMessage()); /**< Network messages concerning npc-status-updates. */
+			MessageListener.Add((byte)NetworkID.MobInterMessage, new MobInterMessage()); /**< Network messages concerning mob interaction (e.g. using an avil). */
 
-            MessageListener.Add((byte)NetworkID.ItemRemovedByUsing, new ItemRemovedByUsing());
-            MessageListener.Add((byte)NetworkID.ContainerItemChangedMessage, new GUC.Server.Network.Messages.ContainerCommands.ItemChangedMessage());
+			MessageListener.Add((byte)NetworkID.ItemRemovedByUsing, new ItemRemovedByUsing()); /**< Network messages concerning item removable following item use events. */
+			MessageListener.Add((byte)NetworkID.ContainerItemChangedMessage, new GUC.Server.Network.Messages.ContainerCommands.ItemChangedMessage()); /**< Network messages concerning container exchange (chests, inventory, etc.). */
 
-            MessageListener.Add((byte)NetworkID.CallbackNPCCanSee, new CallbackNPCCanSee());
-            MessageListener.Add((byte)NetworkID.DoDieMessage, new DoDieMessage());
+			MessageListener.Add((byte)NetworkID.CallbackNPCCanSee, new CallbackNPCCanSee()); /**< Network messages concerning visibility by npcs. */
+			MessageListener.Add((byte)NetworkID.DoDieMessage, new DoDieMessage()); /**< Network messages concerning object/npc/player death. */
 
-            MessageListener.Add((byte)NetworkID.ReadIniEntryMessage, new ReadIniEntryMessage());
-            MessageListener.Add((byte)NetworkID.ReadMd5Message, new ReadMd5Message());
+			MessageListener.Add((byte)NetworkID.ReadIniEntryMessage, new ReadIniEntryMessage()); /**< Network messages concerning ???. */
+			MessageListener.Add((byte)NetworkID.ReadMd5Message, new ReadMd5Message()); /**< Network messages concerning MD5 data encryption. */
 
-            MessageListener.Add((byte)NetworkID.EquipItemMessage, new EquipItemMessage());
+			MessageListener.Add((byte)NetworkID.EquipItemMessage, new EquipItemMessage()); /**< Network messages concerning item equipping. */
 
-            MessageListener.Add((byte)NetworkID.ChangeWorldMessage, new ChangeWorldMessage());
+			MessageListener.Add((byte)NetworkID.ChangeWorldMessage, new ChangeWorldMessage()); /**< Network messages concerning switching the world (go to another world-instance) */
 
-            MessageListener.Add((byte)NetworkID.PlayerKeyMessage, new PlayerKeyMessage());
-            MessageListener.Add((byte)NetworkID.UseItemMessage, new UseItemMessage());
+			MessageListener.Add((byte)NetworkID.PlayerKeyMessage, new PlayerKeyMessage()); /**< Network messages concerning keys triggered/pressed by players. */
+			MessageListener.Add((byte)NetworkID.UseItemMessage, new UseItemMessage()); /**< Network messages concerning item use. */
 
-            MessageListener.Add((byte)NetworkID.CastSpell, new CastSpell());
-            MessageListener.Add((byte)NetworkID.SpellInvestMessage, new SpellInvestMessage());
+			MessageListener.Add((byte)NetworkID.CastSpell, new CastSpell()); /**< Network messages concerning cast spells. */
+			MessageListener.Add((byte)NetworkID.SpellInvestMessage, new SpellInvestMessage()); /**< Network messages concerning investments of an npcs to cast a spell (e.g. mana use). */
 
 
-            MessageListener.Add((byte)NetworkID.PlayerOpenInventoryMessage, new OpenInventoryMessage());
-        }
+			MessageListener.Add((byte)NetworkID.PlayerOpenInventoryMessage, new OpenInventoryMessage());
+		}
 
+        /**
+         *   Initializes/Starts the RakPeerInterface server to actually listen and answer network messages.
+         *   Completes the setup of the network interface/server and starts it, so it can listen on the given port.
+         *   @param port an ushort which is the main port of game server network communication.
+         *   @param maxConnections an ushort which limits the maximum accepted client-server-connections.
+         *   @param pw a String which defines the password to verify client-server-connections (???).
+         */
         public void Start(ushort port, ushort maxConnections, String pw)
         {
             pw = GUC.Options.Constants.VERSION + pw;
@@ -100,6 +112,11 @@ namespace GUC.Server.Network
                 Log.Logger.log("Server start listening on port "+port);
         }
 
+        /**
+         *   Counts the current amount of client-server-connections and returns them.
+         *   Counts the current amount of client-server-connections and returns them.
+         *   @return numbers as ushort amount of client-server-connections
+         */
         public ushort ConnectionCount()
         {
             SystemAddress[] sa = null;
@@ -108,6 +125,14 @@ namespace GUC.Server.Network
             return numbers;
         }
 
+        /**
+         *   Game loop which receives data from clients and redirects/reacts accordingly.
+         *   In this surrounding loop data is received from individual clients and the server reacts depending 
+         *   on the network message types (see class attributes for these types). This is done for each
+         *   network message received by individual clients until there is no more (buffered) message
+         *   left. Character creation is done here on successful connection as well as the respective 
+         *   deletion of disconnect.
+         */
         public void Update()
         {
             Packet p = ServerInterface.Receive();
