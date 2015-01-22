2f60fe9794f0b83987e7657eeaaea01292b4f2f8
Mutton <torpedoente@googlemail.com>
2015-01-22 18:38:42 +0100

    copied Doxgen documentation of network messsages from Server.cs to NetworkIDS.cs; continued documentation fo the left network messages in NetworkIDS.cs
    (cherry picked from commit 4b273ba3f3b18f84f357845deb19c1f0d492b8b7)
    
    Conflicts:
    	GMP_Server/Network/Server.cs
    	Network/Enumeration/NetworkIDS.cs

--- Network/Enumeration/NetworkIDS.cs
+++ Network/Enumeration/NetworkIDS.cs
@@ -5,79 +5,82 @@ using System.Text;
 
 namespace GUC.Enumeration
 {
+    /** 
+    * Types of network messages (data) which can be exchanged between clients and server.
+    */
     public enum NetworkID : byte
     {
-        ConnectionMessage,
-        DisconnectMessage,
+        ConnectionMessage, /**< Network messages concerning player connection state. */
+        DisconnectMessage, /**< Network messages concerning player disconnects. */
 
-        CreateItemInstanceMessage,
-        CreateSpellMessage,
+        CreateItemInstanceMessage, /**< Network messages concerning item instantiation. */
+        CreateSpellMessage, /**< Network messages concerning spell instantiation. */
 
-        PlayerFreezeMessage,
-        AddItemMessage,
-        StartDialogAnimMessage,
-        AnimationMessage,
-        NPCChangeAttributeMessage,
-        NPCChangeSkillMessage,
-        NPCSpawnMessage,
-        GuiMessage,
-        OnDamageMessage,
-        DropUnconsciousMessage,
-        ReviveMessage,
-        SetVisualMessage,
-        SetVobChangeMessage,
-        DropItemMessage,
-        TakeItemMessage,
-        CamToVobMessage,
-        ChangeNameMessage,
-        CreateVobMessage,
-        SpawnVobMessage,
-        DespawnVobMessage,
-        SetVobPositionMessage,
-        SetVobDirectionMessage,
+        PlayerFreezeMessage, /**< Network messages concerning players being frozen (immobile). */
+        AddItemMessage, /**< Network messages concerning adding items to npc inventories. */
+        StartDialogAnimMessage, /**< Network messages concerning animation on dialogue start. */
+        AnimationMessage, /**< Network messages concerning animations (start, stop, change). */
+        NPCChangeAttributeMessage, /**< Network messages concerning attribute changes of npcs. */
+        NPCChangeSkillMessage, /**< Network messages concerning skill changes of npcs. */
+        NPCSpawnMessage, /**< Network messages concerning spawning npcs. */
+        GuiMessage, /**< Network messages concerning GUI events. */
+        OnDamageMessage, /**< Network messages concerning damage events. */
+        DropUnconsciousMessage, /**< Network messages concerning the unconscious state of npcs. */
+        ReviveMessage, /**< Network messages concerning npcs revival. */
+        SetVisualMessage, /**< Network messages concerning changes of npc visuals. */
+		SetVobChangeMessage,
+        DropItemMessage, /**< Network messages concerning events which are triggered when items are dropped into the world. */
+        TakeItemMessage, /**< Network messages concerning events which are triggered when world-items are taken. */
+        CamToVobMessage, /**< Network messages when a camera is set/moved to a vob. */
+        ChangeNameMessage, /**< Network messages concerning name changes of npcs. */
+        CreateVobMessage, /**< Network messages concerning vob creation (not the final spawning process). */
+        SpawnVobMessage, /**< Network messages concerning spawning vobs. */
+		DespawnVobMessage,
+        SetVobPositionMessage, /**< Network messages concerning vob positioning (cartesian coordinates). */
+        SetVobDirectionMessage, /**< Network messages concerning vob orientation/rotation. */
 
-        SetVobPosDirMessage,
-        SetVobListPosDirMessage,
+        SetVobPosDirMessage, /**< Network messages concerning forced vob positioning and rotation (in world). */
+        SetVobListPosDirMessage, /**< ??? Additional message for organizational purposes ???  */
 
-        PlayerUpdateMessage,
-        AnimationUpdateMessage,
-        NPCUpdateMessage,
+        PlayerUpdateMessage, /**< ??? Doesn't seem to have a purpose due to lack of references in whole G:UC solution ??? */
+        AnimationUpdateMessage, /**< Network messages concerning npc animations. */
+        NPCUpdateMessage, /**< Network messages concerning npc-status-updates. */
 
-        MobInterMessage,
-        ItemChangeAmount,
-        ItemRemovedByUsing,
-        ContainerItemChangedMessage,
-        ItemChangeContainer,
+        MobInterMessage, /**< Network messages concerning mob interaction (e.g. using an avil). */
+        ItemChangeAmount, /**< Network messages concerning changing amounts of item stacks. */
+        ItemRemovedByUsing, /**< Network messages concerning item removable following item use events. */
+        ContainerItemChangedMessage, /**< Network messages concerning container exchange (chests, inventory, etc.). */
+        ItemChangeContainer, /**< Network messages when item instances change containers. */
         ClearInventory,
         TimerMessage,
         RainMessage,
-        
-        CallbackNPCCanSee,
-        ReadIniEntryMessage,
-        ReadMd5Message,
 
-        DoDieMessage,
+        CallbackNPCCanSee, /**< Network messages concerning visibility by npcs. */
+        ReadIniEntryMessage, /**< Network messages concerning ???. */
+        ReadMd5Message, /**< Network messages concerning MD5 data encryption. */
+
+        DoDieMessage, /**< Network messages concerning object/npc/player death. */
         ExitGameMessage,
-        EquipItemMessage,
+        EquipItemMessage, /**< Network messages concerning item equipping. */
 
-        ChangeWorldMessage,
+        ChangeWorldMessage, /**< Network messages concerning switching the world (go to another world-instance) */
         NPCSetInvisibleMessage,
         NPCSetInvisibleName,
         PlayVideo,
         NPCControllerMessage,
         ScaleMessage,
         NPCFatnessMessage,
-        
-        PlayerKeyMessage,
+
+        PlayerKeyMessage, /**< Network messages concerning keys triggered/pressed by players. */
 
         NPCProtoSetWeaponMode,
         NPCEnableMessage,
 
-        UseItemMessage,
-        CastSpell,
+        UseItemMessage, /**< Network messages concerning item use. */
+        CastSpell, /**< Network messages concerning cast spells. */
         PlayEffectMessage,
 
-        SpellInvestMessage,
+        SpellInvestMessage, /**< Network messages concerning investments of an npcs to cast a spell (e.g. mana use). */
 
         SetSlotMessage,
         CamToPlayerFront,
