b80455ac9a58a3881a25d37ae2373348f2d25b22
Mutton <torpedoente@googlemail.com>
2015-02-22 19:56:32 +0100

    splitting LoadDefinition-method into loading-functions for different def-tables with splitted sql-queries, too

--- ServerScripts/Sumpfkraut/VobSystem/VobHandler.cs
+++ ServerScripts/Sumpfkraut/VobSystem/VobHandler.cs
@@ -39,7 +39,7 @@ namespace GUC.Server.Scripts.Sumpfkraut.VobSystem
          *   @param defTab , the enum-entry which represents the type of definitions to load
          *   @see DefTableEnum
          */
-        private static void loadDefinition(DefTableEnum defTab)
+        public static void loadDefinition (DefTableEnum defTab)
         {
             if ((defTab == null) || (!DBTables.DefTableDict.ContainsKey(defTab)))
             {
@@ -68,11 +68,11 @@ namespace GUC.Server.Scripts.Sumpfkraut.VobSystem
 
                     Dictionary<String, SQLiteGetTypeEnum> colTypes = null;
                     DBTables.DefTableDict.TryGetValue(defTab, out colTypes);
-                    //List<string> colKeys = new List<string>(colTypes.Count());
-                    string[] colKeys = new string[colTypes.Count()];
-                    //List<object> colVals = new List<object>(colTypes.Count());
-                    object[] colVals = new object[colTypes.Count()];
-                    bool hasEffects = false; 
+                    List<string> colKeys = new List<string>();
+                    List<object> colVals = new List<object>();
+                    //string[] colKeys = new string[colTypes.Count()];
+                    //object[] colVals = new object[colTypes.Count()];
+                    //bool hasEffects = false; 
 
                     int col = 0;
                     while (rdr.Read())
@@ -80,27 +80,119 @@ namespace GUC.Server.Scripts.Sumpfkraut.VobSystem
                         col = 0;
                         foreach(KeyValuePair<string, SQLiteGetTypeEnum> e in colTypes)
                         {
-                            colKeys[col] = e.Key;
-                            colVals[col] = DBTables.SqlReadType(ref rdr, col, e.Value);
-                            if ((colKeys[col] == "HasEffects") && ((bool) colVals[col]))
-                            {
-                                hasEffects = true;
-                            }
+                            colKeys.Add(e.Key);
+                            colVals.Add(e.Value);
+                            //colKeys[col] = e.Key;
+                            //colVals[col] = DBTables.SqlReadType(ref rdr, col, e.Value);
+                            //if ((colKeys[col] == "HasEffects") && ((bool) colVals[col]))
+                            //{
+                            //    hasEffects = true;
+                            //}
                             col++;
                         }
 
                         // TO DO: Forward data into functionality which applies changes in various ways
                         // for vob definitions, the effect and effect-changes definitons have to be loaded
                         // directly one after the other!!!
-                        if (hasEffects)
-                        {
 
-                        }
+                        //bool hasEffects = Convert.ToBoolean(colVals[colKeys.IndexOf("HasEffects")]);
+                        //int mobDefID = Convert.ToInt32(colVals[colKeys.IndexOf("MobDefID")]);
+                        //int effectDefID = -1;
+
+                        //if ((mobDefID != null) && (hasEffects != null) && (hasEffects))
+                        //{
+                        //    cmd.CommandText = "SELECT * FROM `MobDef_Effects_inst` WHERE ID=" + mobDefID + ";";
+                        //    try
+                        //    {
+                        //        rdr = cmd.ExecuteReader();
+                        //        if (!rdr.HasRows)
+                        //        {
+                        //            return;
+                        //        }
+                        //    }
+                        //    catch (Exception ex)
+                        //    {
+                        //        throw new Exception("Could not execute SQLiteDataReader during effect-definiton-loading: " + ex);
+                        //    }
+                        //    finally
+                        //    {
+                        //        if (rdr != null)
+                        //        {
+                        //            rdr.Close();
+                        //        }
+                        //    }
+                        //}
                     }
                 }
                 catch (Exception ex)
                 {
                     throw new Exception("Could not execute SQLiteDataReader during vob-definiton-loading: " + ex);
+                }
+                finally
+                {
+                    if (rdr != null)
+                    {
+                        rdr.Close();
+                    }
+                }
+            }
+        }
+
+        private static void LoadVobDef ()
+        {
+
+        }
+
+        private static void LoadEffectDef (out List<int> colKeys, out List<object> colVals, 
+            ref List<int> ids)
+        {
+            colKeys = new List<int>();
+            colVals = new List<object>();
+        }
+
+
+        // must be able to read multiple rows 
+        // count of colVals multiple of the count of colKeys to prevent unnecessary repetition?
+        // 1 => object, 2 => object, 3 => object, 4 => object, ... , 1 => object, ...
+        private static void LoadEffectChangesDef (out List<int> colKeys, out List<object> colVals, 
+            ref List<int> ids)
+        {
+            colKeys = new List<int>();
+            colVals = new List<object>();
+
+            if ((ids == null) || (ids.Count <= 0))
+            {
+                return;
+            }
+
+            using (SQLiteCommand cmd = new SQLiteCommand(Sqlite.getSqlite().connection))
+            {
+                cmd.CommandText = "SELECT * FROM `Effect_Changes_def` WHERE `ID` IN (" 
+                    + String.Join(",", ids.ToArray()) 
+                    + ");";
+                SQLiteDataReader rdr = null;
+                try
+                {
+                    rdr = cmd.ExecuteReader();
+                    if (!rdr.HasRows)
+                    {
+                        return;
+                    }
+
+                    int col = 0;
+                    while(rdr.Read())
+                    {
+                        //col = 0;
+                        //foreach(KeyValuePair<string, SQLiteGetTypeEnum> e in colTypes)
+                        //{
+
+                        //    col++;
+                        //}
+                    }
+                }
+                catch (Exception ex)
+                {
+                    throw new Exception("Could not execute SQLiteDataReader during loading of effect changes definitions: " + ex);
                 }
                 finally
                 {
