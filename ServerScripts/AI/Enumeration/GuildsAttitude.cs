b3758e97a8a27e97e97728cf04e32eecb1c880c3
Lehona <Leonvonmulert@gmail.com>
2015-03-05 19:26:23 +0100

    Misc documentation

--- ServerScripts/AI/Enumeration/GuildsAttitude.cs
+++ ServerScripts/AI/Enumeration/GuildsAttitude.cs
@@ -5,10 +5,14 @@ using System.Text;
 
 namespace GUC.Server.Scripts.AI.Enumeration
 {
+
+    /**
+     * AI-Enum
+     */
     public enum GuildsAttitude
     {
-        HOSTILE,
-        NEUTRAL,
-        FRIENDLY
+        HOSTILE, /**< Guilds which are hostile to each other will kill/attack on sight.*/
+        NEUTRAL, /**< Guilds which are neutral to each other will largely ignore each other. Attacking anyone will make a neutral guild temporarily hostile.*/
+        FRIENDLY /**< Guilds which are friendly to each other will help when a member is attacked.*/
     }
 }
