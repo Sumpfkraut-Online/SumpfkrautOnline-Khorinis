cb141de5c43d3a8c67603a5119268eddc30d5dac
CrimsonGhost <whocares@trash-mail.com>
2015-03-05 16:08:57 +0100

    resolve problems introduced with the merge

--- Network/WorldObjects/Character/NPCProto.cs
+++ Network/WorldObjects/Character/NPCProto.cs
@@ -226,7 +226,7 @@ namespace GUC.WorldObjects.Character
             if (oldItem != null && item.ItemInstance.Flags.HasFlag(Flags.ITEM_MULTI))//Delete Old Item
             {
                 item.Map = null;
-                sWorld.removeVob(item);
+                sWorld.getWorld(this.Map).removeVob(item);
 
                 oldItem.Amount += item.Amount;
             }
