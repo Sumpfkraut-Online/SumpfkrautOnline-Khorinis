d78363ffdb930a4494bbe8897b83fe6989d55cf5
Mutton <torpedoente@googlemail.com>
2015-03-20 17:55:41 +0100

    completed LoadFromDB-method while having excluded the data conversion (must be done smewhere else now)

--- ServerScripts/Sumpfkraut/Database/DBReader.cs
+++ ServerScripts/Sumpfkraut/Database/DBReader.cs
@@ -11,52 +11,61 @@ namespace GUC.Server.Scripts.Sumpfkraut.Database
     class DBReader
     {
 
+        ///**
+        // *   Executes a defined sql-query and stores the results as strings.
+        // *   Stores the results for each sql-statement provided, so that after defining 2 statements 
+        // *   (statement ends with ;) there is a list of 2 results. Each result ist in itself a list of rows.
+        // *   Per row there can be 0 or more objects/table column entries of data.
+        // *   @param results stores the results of 1 or multuple provided sql-queries
+        // *   @param colTypes is the list which provides ways to convert the resulting strings to more defined server data
+        // *   @param colTypesKeys stores the column names of the data table in a numeric manner (used in cata conversion)
+        // *   @param colTypesVals stores the hints on data conversion used in a numeric manner (used in cata conversion)
+        // *   @param convertData must be true if the resultings strings should be converted to data on load
+        // *   @param select is the optional string after the SELECT-statement in sql
+        // *   @param from is the optional string after the FROM-statement in sql
+        // *   @param where is the optional string after the WHERE-statement in sql
+        // *   @param orderBy is the optional string after the ORDER BY-statement in sql
+        // *   @param completeQuery is an optional query which can be used to define more than one sql-statement in one shot (if provided, select, from, where, orderBy are ignored)
+        // */
+
         /**
-         *   Executes a defined sql-query and stores the results.
+         *   Executes a defined sql-query and stores the results as strings.
          *   Stores the results for each sql-statement provided, so that after defining 2 statements 
          *   (statement ends with ;) there is a list of 2 results. Each result ist in itself a list of rows.
          *   Per row there can be 0 or more objects/table column entries of data.
          *   @param results stores the results of 1 or multuple provided sql-queries
-         *   @param colTypes is the list which provides ways to convert the resulting strings to more defined server data
-         *   @param colTypesKeys stores the column names of the data table in a numeric manner (used in cata conversion)
-         *   @param colTypesVals stores the hints on data conversion used in a numeric manner (used in cata conversion)
-         *   @param convertData must be true if the resultings strings should be converted to data on load
          *   @param select is the optional string after the SELECT-statement in sql
          *   @param from is the optional string after the FROM-statement in sql
          *   @param where is the optional string after the WHERE-statement in sql
          *   @param orderBy is the optional string after the ORDER BY-statement in sql
-         *   @param completeQuery is an optional query which can be used to define more than one sql-statement in one shot (if provided, select, from, where, orderBy are ignored)
          */
-        public static void LoadFromDB (
-            out List<List<List<object>>> results,
-            ref Dictionary<String, SQLiteGetTypeEnum> colTypes,
-            out List<string> colTypesKeys, out List<SQLiteGetTypeEnum> colTypesVals, 
-            bool convertData = true,
-            string select="", string from="", string where="", string orderBy="",
-            string completeQuery = "")
-        {
-            // the actual query results
-            results = new List<List<List<object>>>();
+        public static void LoadFromDB (ref List<List<List<object>>> results,
+            string select = "", string from = "", string where = "", string orderBy = "")
+        { 
+            string completeQuery = "SELECT " + select
+                + " FROM " + from
+                + " WHERE " + where
+                + " ORDER BY " + orderBy
+                + ";";
 
-            // lists to ensure same key-value-order for each row in rdr because, otherwise, the memory
-            // adresses of the original dictionary and order might be changed during runtime
-            colTypesKeys = new List<string>(colTypes.Keys);
-            colTypesVals = new List<SQLiteGetTypeEnum>(colTypes.Values);
+            LoadFromDB(ref results, completeQuery);
+        }
+
+        /**
+         *   Executes a defined sql-query and stores the results as strings.
+         *   Stores the results for each sql-statement provided, so that after defining 2 statements 
+         *   (statement ends with ;) there is a list of 2 results. Each result ist in itself a list of rows.
+         *   Per row there can be 0 or more objects/table column entries of data.
+         *   @param results stores the results of 1 or multiple provided sql-queries
+         *   @param completeQuery is the query which is send to the database (can have multiple statements seperated by ;)
+         */
+        public static void LoadFromDB (ref List<List<List<object>>> results,
+            string completeQuery)
+        {
 
             using (SQLiteCommand cmd = new SQLiteCommand(Sqlite.getSqlite().connection))
             {
-                if (completeQuery.Length <= 0)
-                {
-                    cmd.CommandText = "SELECT " + select
-                    + " FROM " + from
-                    + " WHERE " + where
-                    + " ORDER BY " + orderBy
-                    + ";";
-                }
-                else
-                {
-                    cmd.CommandText = completeQuery;
-                }
+                cmd.CommandText = completeQuery;
 
                 SQLiteDataReader rdr = null;
                 try
@@ -67,8 +76,8 @@ namespace GUC.Server.Scripts.Sumpfkraut.Database
                         return;
                     }
 
-                    // temporary list to put all data of a row into
-                    List<object> rowList = null;
+                    // temporary array to put all data of a row into
+                    object[] rowArr = null;
 
                     do
                     {
@@ -76,14 +85,10 @@ namespace GUC.Server.Scripts.Sumpfkraut.Database
                         results.Add(new List<List<object>>());
                         while (rdr.Read())
                         {
-                            // create and fill list of the temporary data row
-                            rowList = new List<object>();
-                            for (int col = 0; col < colTypesKeys.Count; col++)
-                            {
-                                rowList.Add(DBTables.SqlReadType(ref rdr, col, colTypesVals[col]));
-                            }
-                            // add the complete temporary data row to the current result-list
-                            results[results.Count - 1].Add(rowList);
+                            // create and fill array of the temporary data row
+                            rowArr = new object[rdr.FieldCount];
+                            rdr.GetValues(rowArr);
+                            results[results.Count - 1].Add(new List<object>(rowArr));
                         }
                     }
                     while (rdr.NextResult());
