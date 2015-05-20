6c55dd30b7498da8d94a1ac7b5b1978191c7b83c
Mutton <torpedoente@googlemail.com>
2015-05-20 17:27:38 +0200

    corrected inventory-item-handlig to only use ItemDef (the ItemInst are less accessable than just saying >>does it have this KIND of item in that amount<<)

--- ServerScripts/Sumpfkraut/VobSystem/InventoryVobHandler.cs
+++ ServerScripts/Sumpfkraut/VobSystem/InventoryVobHandler.cs
@@ -133,20 +133,13 @@ namespace GUC.Server.Scripts.Sumpfkraut.VobSystem
             return true;
         }
 
-        public static bool HasItems (int npcInstID, int[] itemInstIDs, int[] amounts, out int[] errRealAmounts)
-        {
-            errRealAmounts = new int[1];
-            bool hasItems = true;
-            return hasItems;
-        }
-
-        public static bool HasItemsByInst (int npcInstID, int[] itemInstIDs, int[] amounts, out int[] errRealAmounts)
+        public static bool HasItems (int npcInstID, int[] itemDefIDs, int[] amounts, out int[] errRealAmounts)
         {
             bool hasItems = true;
-            errRealAmounts = new int[itemInstIDs.Length];
+            errRealAmounts = new int[itemDefIDs.Length];
 
             // ---------------------------------------------------------------------------------
-            // start with searching the inventory of NPCInst for ItemInst fitting the given IDs
+            // start with requesting the whole inventory of NPCInst
             // ---------------------------------------------------------------------------------
 
             // get the keys and values for the database-table individually but ensure same order
@@ -162,9 +155,7 @@ namespace GUC.Server.Scripts.Sumpfkraut.VobSystem
             inInv_completeQuerySB.Append(Utilities.String.Concatenate<string>(inInv_colTypesKeys, ","));
             inInv_completeQuerySB.Append(" FROM `ItemInInventory_inst` WHERE `NPCInstID`=");
             inInv_completeQuerySB.Append(npcInstID);
-            inInv_completeQuerySB.Append(" AND `ItemInstID` IN (");
-            inInv_completeQuerySB.Append(Utilities.String.Concatenate<int>(itemInstIDs, ","));
-            inInv_completeQuerySB.Append(") ORDER BY `ItemInstID` ASC;");
+            inInv_completeQuerySB.Append(" ORDER BY `ItemInstID` ASC;");
             string inInv_completeQuery = inInv_completeQuerySB.ToString();
 
             List<List<List<object>>> inInv_sqlResults = new List<List<List<object>>>();
@@ -182,12 +173,6 @@ namespace GUC.Server.Scripts.Sumpfkraut.VobSystem
             for ( int r = 0; r < inInv_sqlResults[0].Count; r++ )
             {
                 inInv_itemInstIDs[r] = (int) inInv_sqlResults[0][r][inInv_ItemInstIDIndex];
-            }
-
-            if (inInv_itemInstIDs.Length < itemInstIDs.Length)
-            {
-                // if not all searched ids are found, there must be some missing completely
-                hasItems = false;
             }
 
             // ---------------------------------------------------------------------------------
@@ -226,12 +211,12 @@ namespace GUC.Server.Scripts.Sumpfkraut.VobSystem
             {
                 // get the index in original order with all expected itemInstIDs
                 // (including those not found at all after search in inventory)
-                tempIndex = Array.IndexOf<int>(itemInstIDs, (int) itemInst_sqlResults[0][r][itemInst_IDIndex]);
+                tempIndex = Array.IndexOf<int>(itemDefIDs, (int) itemInst_sqlResults[0][r][itemInst_ItemDefIDIndex]);
 
                 // update the array of real found item amounts for error analysis by user
                 errRealAmounts[tempIndex] = (int) itemInst_sqlResults[0][r][itemInst_AmountIndex];
 
-                if (errRealAmounts[tempIndex] < itemInstIDs[tempIndex])
+                if (errRealAmounts[tempIndex] < itemDefIDs[tempIndex])
                 {
                     // if only one of all those items doesn't fulfill its necessary amount, the whole check will be false
                     hasItems = false;
@@ -240,6 +225,107 @@ namespace GUC.Server.Scripts.Sumpfkraut.VobSystem
 
             return hasItems;
         }
+
+        //public static bool HasItemsByInst (int npcInstID, int[] itemInstIDs, int[] amounts, out int[] errRealAmounts)
+        //{
+        //    bool hasItems = true;
+        //    errRealAmounts = new int[itemInstIDs.Length];
+
+        //    // ---------------------------------------------------------------------------------
+        //    // start with searching the inventory of NPCInst for ItemInst fitting the given IDs
+        //    // ---------------------------------------------------------------------------------
+
+        //    // get the keys and values for the database-table individually but ensure same order
+        //    List<string> inInv_colTypesKeys = new List<string>(itemInInvTableDict.Keys);
+        //    List<SQLiteGetTypeEnum> inInv_colTypesVals = new List<SQLiteGetTypeEnum>();
+        //    for (int i = 0; i < inInv_colTypesKeys.Count; i++)
+        //    {
+        //        inInv_colTypesVals.Add( itemInInvTableDict[inInv_colTypesKeys[i]] );
+        //    }
+
+        //    System.Text.StringBuilder inInv_completeQuerySB = new System.Text.StringBuilder();
+        //    inInv_completeQuerySB.Append("SELECT ");
+        //    inInv_completeQuerySB.Append(Utilities.String.Concatenate<string>(inInv_colTypesKeys, ","));
+        //    inInv_completeQuerySB.Append(" FROM `ItemInInventory_inst` WHERE `NPCInstID`=");
+        //    inInv_completeQuerySB.Append(npcInstID);
+        //    inInv_completeQuerySB.Append(" AND `ItemInstID` IN (");
+        //    inInv_completeQuerySB.Append(Utilities.String.Concatenate<int>(itemInstIDs, ","));
+        //    inInv_completeQuerySB.Append(") ORDER BY `ItemInstID` ASC;");
+        //    string inInv_completeQuery = inInv_completeQuerySB.ToString();
+
+        //    List<List<List<object>>> inInv_sqlResults = new List<List<List<object>>>();
+        //    Database.DBReader.LoadFromDB(ref inInv_sqlResults, inInv_completeQuery);
+
+        //    if ((inInv_sqlResults.Count < 1) || (inInv_sqlResults[0].Count < 1))
+        //    {
+        //        // no such items in inventory
+        //        // errRealAmounts will be filled solely with zeros
+        //        return false;
+        //    }
+
+        //    int inInv_ItemInstIDIndex = inInv_colTypesKeys.IndexOf("ItemInstID");
+        //    int[] inInv_itemInstIDs = new int[inInv_sqlResults[0].Count];
+        //    for ( int r = 0; r < inInv_sqlResults[0].Count; r++ )
+        //    {
+        //        inInv_itemInstIDs[r] = (int) inInv_sqlResults[0][r][inInv_ItemInstIDIndex];
+        //    }
+
+        //    if (inInv_itemInstIDs.Length < itemInstIDs.Length)
+        //    {
+        //        // if not all searched ids are found, there must be some missing completely
+        //        hasItems = false;
+        //    }
+
+        //    // ---------------------------------------------------------------------------------
+        //    // continue with checking the amounts of all ItemInst in Item_inst-database-table
+        //    // ---------------------------------------------------------------------------------
+
+        //    // get the keys and values for the database-table individually but ensure same order
+        //    List<string> itemInst_colTypesKeys = new List<string>(itemInInvTableDict.Keys);
+        //    List<SQLiteGetTypeEnum> itemInst_colTypesVals = new List<SQLiteGetTypeEnum>();
+        //    for (int i = 0; i < itemInst_colTypesKeys.Count; i++)
+        //    {
+        //        itemInst_colTypesVals.Add( itemInstTableDict[itemInst_colTypesKeys[i]] );
+        //    }
+
+        //    System.Text.StringBuilder itemInst_completeQuerySB = new System.Text.StringBuilder();
+        //    itemInst_completeQuerySB.Append("SELECT ");
+        //    itemInst_completeQuerySB.Append(Utilities.String.Concatenate<string>(itemInst_colTypesKeys, ","));
+        //    itemInst_completeQuerySB.Append(" FROM `Item_inst` WHERE `ID` IN (");
+        //    itemInst_completeQuerySB.Append(Utilities.String.Concatenate<int>(inInv_itemInstIDs, ","));
+        //    itemInst_completeQuerySB.Append(") ORDER BY `ID` ASC;");
+        //    string itemInst_completeQuery = itemInst_completeQuerySB.ToString();
+
+        //    List<List<List<object>>> itemInst_sqlResults = new List<List<List<object>>>();
+        //    Database.DBReader.LoadFromDB(ref itemInst_sqlResults, itemInst_completeQuery);
+
+        //    if ((itemInst_sqlResults.Count < 1) || (itemInst_sqlResults[0].Count < 1))
+        //    {
+        //        // shouldn't happen when the database FOREIGN KEY-constraints are set up correctly :P
+        //    }
+
+        //    int itemInst_IDIndex = itemInst_colTypesKeys.IndexOf("ID");
+        //    int itemInst_ItemDefIDIndex = itemInst_colTypesKeys.IndexOf("ItemDefID");
+        //    int itemInst_AmountIndex = itemInst_colTypesKeys.IndexOf("Amount");
+        //    int tempIndex = 0;
+        //    for (int r = 0; r < itemInst_sqlResults[0].Count; r++)
+        //    {
+        //        // get the index in original order with all expected itemInstIDs
+        //        // (including those not found at all after search in inventory)
+        //        tempIndex = Array.IndexOf<int>(itemInstIDs, (int) itemInst_sqlResults[0][r][itemInst_IDIndex]);
+
+        //        // update the array of real found item amounts for error analysis by user
+        //        errRealAmounts[tempIndex] = (int) itemInst_sqlResults[0][r][itemInst_AmountIndex];
+
+        //        if (errRealAmounts[tempIndex] < itemInstIDs[tempIndex])
+        //        {
+        //            // if only one of all those items doesn't fulfill its necessary amount, the whole check will be false
+        //            hasItems = false;
+        //        }
+        //    }
+
+        //    return hasItems;
+        //}
 
     }
 }
