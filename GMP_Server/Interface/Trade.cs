fa3c2aa32ee034b9220b05b6f86313f27c1498c1
MadShadow <Mad.Shadow@web.de>
2015-11-04 00:52:44 +0100

    Added TradeMessage.cs/Trade.cs to Server
    - Dateien erstellt

---
+++ GMP_Server/Interface/Trade.cs
@@ -0,0 +1,234 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using GUC.Network;
+using GUC.Server.WorldObjects;
+using GUC.Server.Network.Messages;
+
+namespace GUC.Server.Interface
+{
+    public class Trade
+    {
+        private static Trade tradeCtrl;
+        public static Trade GetTrade()
+        {
+            if (tradeCtrl == null)
+            {
+                tradeCtrl = new Trade();
+            }
+            return tradeCtrl;
+        }
+
+        private List<Request> requests;
+        private List<TradeCouple> tradings;
+
+        public Trade()
+        {
+            requests = new List<Request>();
+            tradings = new List<TradeCouple>();
+        }
+
+
+        private class Request
+        {
+            public NPC requester;
+            public NPC target;
+            public long time;
+
+            public Request(NPC requester, NPC target)
+            {
+                Log.Logger.log("created new request");
+                this.requester = requester;
+                this.target = target;
+                time = DateTime.Now.Ticks + 300000000; // 30 secs
+            }
+        }
+
+        private class TradeCouple
+        {
+            public NPC pl1;
+            public List<Item> list1;
+            public NPC pl2;
+            public List<Item> list2;
+            public bool accepted1;
+            public bool accepted2;
+
+            public TradeCouple(NPC p1, NPC p2)
+            {
+                pl1 = p1; pl2 = p2;
+                list1 = new List<Item>();
+                list2 = new List<Item>();
+                accepted1 = false;
+                accepted2 = false;
+            }
+        }
+
+        public void OnRequestMessage(NPC requester, NPC target)
+        {
+            Log.Logger.log("request by " + target.ID.ToString() + " to " + requester.ID.ToString());
+
+            // target already in trade?
+            foreach (TradeCouple tc in tradings)
+            {
+                if (tc.pl1 == target || tc.pl2 == target)
+                {
+                    return;
+                }
+
+                if (tc.pl1 == requester || tc.pl2 == requester)
+                {
+                    return;
+                }
+            }
+
+            CleanRequestList();
+
+            foreach (Request req in requests)
+            {
+                if (req.requester == target && req.target == requester)
+                {
+                    Log.Logger.log("request accepted -> send message");
+                    AddCouple(requester, target);
+                    requests.Remove(req);
+                    TradeMessage.SendAccept(requester, target);
+                    return;
+                }
+                else if (req.requester == requester && req.target == target)
+                {
+                    req.time = DateTime.Now.Ticks + 300000000; //30sec long
+                    return;
+                }
+            }
+
+            // no request so far -> create new request
+            Request request = new Request(requester, target);
+            requests.Add(request);
+        }
+
+        public void OnBreakMessage(NPC sender)
+        {
+            for (int i = tradings.Count - 1; i >= 0; i--)
+            {
+                if (tradings[i].pl1 == sender)
+                {
+                    TradeMessage.SendBreak(tradings[i].pl2);
+                    tradings.Remove(tradings[i]);
+                }
+                else if (tradings[i].pl2 == sender)
+                {
+                    TradeMessage.SendBreak(tradings[i].pl1);
+                    tradings.Remove(tradings[i]);
+                }
+            }
+        }
+
+        public void OfferDeclined(NPC sender, bool send)
+        {
+            for (int i = tradings.Count - 1; i >= 0; i--)
+            {
+                if (tradings[i].pl1 == sender)
+                {
+                    tradings[i].accepted1 = false;
+                    if (send)
+                    {
+                        TradeMessage.SendOfferDeclined(tradings[i].pl2);
+                    }
+                }
+                else if (tradings[i].pl2 == sender)
+                {
+                    tradings[i].accepted2 = false;
+                    if (send)
+                    {
+                        TradeMessage.SendOfferDeclined(tradings[i].pl1);
+                    }
+                }
+            }
+        }
+
+        public void OfferConfirmed(NPC sender)
+        {
+            for (int i = tradings.Count - 1; i >= 0; i--)
+            {
+                if (tradings[i].pl1 == sender)
+                {
+                    tradings[i].accepted1 = true;
+                    TradeMessage.SendOfferConfirmed(tradings[i].pl2);
+                }
+                else if (tradings[i].pl2 == sender)
+                {
+                    tradings[i].accepted2 = true;
+                    TradeMessage.SendOfferConfirmed(tradings[i].pl1);
+                }
+
+                if(tradings[i].accepted1 && tradings[i].accepted2)
+                {
+                    Log.Logger.log("both accepted the trade");
+                    // trade has been accepted by both players
+                }
+            }
+        }
+
+        public void OnOfferMessage(NPC trader, Item item, bool add)
+        {
+            Log.Logger.log(trader.ID.ToString() + " offers " + item.Instance.Name.ToString());
+            if (!trader.HasItem(item))
+                return;
+
+            foreach (TradeCouple tc in tradings)
+            {
+                if (trader == tc.pl1)
+                {
+                    //if (tc.accepted1 || tc.accepted2)
+                    //    return;
+                    if (add && !tc.list1.Contains(item))
+                    {
+                        tc.list1.Add(item);
+                    }
+                    else if (tc.list1.Contains(item))
+                    {
+                        tc.list1.Remove(item);
+                    }
+                    tc.accepted2 = false;
+                    TradeMessage.SendOffer(tc.pl1, tc.pl2, item, add);
+                    return;
+                }
+                else if (trader == tc.pl2)
+                {
+                   // if (tc.accepted1 || tc.accepted2)
+                   //     return;
+                    if (add && !tc.list2.Contains(item))
+                    {
+                        tc.list2.Add(item);
+                    }
+                    else if (tc.list2.Contains(item))
+                    {
+                        tc.list2.Remove(item);
+                    }
+                    tc.accepted1 = false;
+                    TradeMessage.SendOffer(tc.pl2, tc.pl1, item, add);
+                    return;
+                }
+            }
+        }
+
+        private void CleanRequestList()
+        {
+            long now = DateTime.Now.Ticks;
+            for (int i = requests.Count - 1; i >= 0; i--)
+            {
+                if (requests[i].time < now)
+                    requests.Remove(requests[i]);
+            }
+        }
+
+        private void AddCouple(NPC pl1, NPC pl2)
+        {
+            for (int i = requests.Count - 1; i >= 0; i--)
+                if (requests[i].requester == pl1 || requests[i].target == pl1 || requests[i].requester == pl2 || requests[i].target == pl2)
+                    requests.Remove(requests[i]);
+
+            tradings.Add(new TradeCouple(pl1, pl2));
+        }
+    }
+}
---
+++ GMP_Server/Network/Messages/TradeMessage.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using RakNet;
+using GUC.Network;
+using GUC.Server.WorldObjects;
+using GUC.Enumeration;
+using GUC.Server.Interface;
+
+namespace GUC.Server.Network.Messages
+{
+    static class TradeMessage
+    {
+        public static void Read(BitStream stream, Client client)
+        {
+            byte b = stream.mReadByte();
+            TradeStatus status = (TradeStatus)b;
+            Trade trade = Trade.GetTrade();
+
+            if (status == TradeStatus.Request)
+            {
+                uint targetID = stream.mReadUInt();
+                NPC requester = client.character;
+                NPC target;
+                Log.Logger.log("request by "+targetID.ToString());
+                client.character.World.PlayerDict.TryGetValue(targetID, out target);
+
+                if (requester != null && target != null)
+                {
+                    trade.OnRequestMessage(requester, target);
+                }
+            }
+            else if (status == TradeStatus.Break)
+            {
+                NPC sender = client.character;
+                if (sender != null)
+                {
+                    trade.OnBreakMessage(sender);
+                }
+            }
+            else if(status == TradeStatus.ConfirmOffer)
+            {
+                trade.OfferConfirmed(client.character);
+            }
+            else if(status == TradeStatus.DeclineOffer)
+            {
+                trade.OfferDeclined(client.character, true);
+            }
+            else if (status >= TradeStatus.SelfOfferItem)
+            {
+                trade.OfferDeclined(client.character, false);
+
+                ushort id = stream.mReadUShort();
+                Item item = sWorld.ItemDict[id];
+                NPC sender = client.character;
+
+                if (sender != null && item != null)
+                {
+                    trade.OnOfferMessage(sender, item, status == TradeStatus.SelfOfferItem);
+                }
+            }
+        }
+
+        public static void SendOffer(NPC from, NPC to, Item item, bool add)
+        {
+            BitStream stream = Program.server.SetupStream(NetworkID.TradeMessage);
+            if (add) stream.mWrite((byte)TradeStatus.SelfOfferItem);
+            else stream.mWrite((byte)TradeStatus.SelfRemoveItem);
+            stream.mWrite(item.ID); // back to from ->self offer/remove
+            stream.mWrite(item.Instance.ID);
+            stream.mWrite(item.Amount);
+            stream.mWrite(item.Condition);
+            stream.mWrite(item.SpecialLine);
+            Program.server.ServerInterface.Send(stream, PacketPriority.LOW_PRIORITY, PacketReliability.RELIABLE_ORDERED, 'I', from.client.guid, false);
+
+            stream = Program.server.SetupStream(NetworkID.TradeMessage);
+            if (add) stream.mWrite((byte)TradeStatus.OtherOfferItem);
+            else stream.mWrite((byte)TradeStatus.OtherRemoveItem);
+            stream.mWrite(item.ID); //other offer/remove
+            stream.mWrite(item.Instance.ID);
+            stream.mWrite(item.Amount);
+            stream.mWrite(item.Condition);
+            stream.mWrite(item.SpecialLine);
+            Program.server.ServerInterface.Send(stream, PacketPriority.LOW_PRIORITY, PacketReliability.RELIABLE_ORDERED, 'I', to.client.guid, false);
+        }
+
+        public static void SendAccept(NPC pl1, NPC pl2)
+        {
+            BitStream stream = Program.server.SetupStream(NetworkID.TradeMessage);
+            stream.mWrite((byte)TradeStatus.Accept);
+            stream.mWrite(pl1.ID);
+            Program.server.ServerInterface.Send(stream, PacketPriority.LOW_PRIORITY, PacketReliability.RELIABLE_ORDERED, 'I', pl1.client.guid, false);
+
+            stream = Program.server.SetupStream(NetworkID.TradeMessage);
+            stream.mWrite((byte)TradeStatus.Accept);
+            stream.mWrite(pl2.ID);
+            Program.server.ServerInterface.Send(stream, PacketPriority.LOW_PRIORITY, PacketReliability.RELIABLE_ORDERED, 'I', pl2.client.guid, false);
+        }
+
+        public static void SendBreak(NPC pl)
+        {
+            BitStream stream = Program.server.SetupStream(NetworkID.TradeMessage);
+            stream.mWrite((byte)TradeStatus.Break);
+            stream.mWrite(pl.ID);
+            Program.server.ServerInterface.Send(stream, PacketPriority.LOW_PRIORITY, PacketReliability.RELIABLE_ORDERED, 'I', pl.client.guid, false);
+        }
+
+        public static void SendOfferConfirmed(NPC pl)
+        {
+            BitStream stream = Program.server.SetupStream(NetworkID.TradeMessage);
+            stream.mWrite((byte)TradeStatus.ConfirmOffer);
+            Program.server.ServerInterface.Send(stream, PacketPriority.LOW_PRIORITY, PacketReliability.RELIABLE_ORDERED, 'I', pl.client.guid, false);
+        }
+
+        public static void SendOfferDeclined(NPC pl)
+        {
+            BitStream stream = Program.server.SetupStream(NetworkID.TradeMessage);
+            stream.mWrite((byte)TradeStatus.DeclineOffer);
+            Program.server.ServerInterface.Send(stream, PacketPriority.LOW_PRIORITY, PacketReliability.RELIABLE_ORDERED, 'I', pl.client.guid, false);
+        }
+
+    }
+}
