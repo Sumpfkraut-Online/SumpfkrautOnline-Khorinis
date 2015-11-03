ff4268a902d22876f6bcabcdfefc8ed159e023aa
MadShadow <Mad.Shadow@web.de>
2015-11-04 00:54:16 +0100

    Added TradeMenu.cs/TradeMessages to client
    - Dateien erstellt

---
+++ GMP/Menus/TradeMenu.cs
@@ -0,0 +1,221 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using WinApi;
+using WinApi.User.Enumeration;
+using GUC.Types;
+using GUC.Network;
+using GUC.Enumeration;
+using GUC.Client.WorldObjects;
+using GUC.Client.GUI;
+using GUC.Client.Network.Messages;
+using Gothic.mClasses;
+using Gothic.zClasses;
+using RakNet;
+
+
+namespace GUC.Client.Menus
+{
+    class TradeMenu : GUCMenu
+    {
+
+        private static TradeMenu trade;
+        public static TradeMenu GetTrade()
+        {
+            if (trade == null)
+            {
+                trade = new TradeMenu();
+            }
+            return trade;
+        }
+
+        public VirtualKeys HotKey = VirtualKeys.T;
+        public VirtualKeys AcceptTradeKey = VirtualKeys.T;
+
+        GUCInventory inv;
+        GUCInventory sellInv;
+        GUCInventory buyInv;
+
+        Dictionary<uint, Item> itemsInv;
+        Dictionary<uint, Item> itemsBuy;
+        Dictionary<uint, Item> itemsSell;
+
+        bool confirmed = false;
+        bool otherConfirmed = false;
+
+        public TradeMenu()
+        {
+            itemsBuy = new Dictionary<uint, Item>();
+            itemsSell = new Dictionary<uint, Item>();
+
+            buyInv = new GUCInventory(80, 200, 2, 3); //, "Inv_Back_Sell.tga"); 
+            sellInv = new GUCInventory(290, 200, 2, 3); //, "Inv_Back_Sell.tga"); 
+            inv = new GUCInventory(500, 200, 4, 3); //, "Inv_Back.tga");
+
+            sellInv.SetContents(itemsSell);
+            buyInv.SetContents(itemsBuy);
+
+            inv.left = sellInv;
+            sellInv.right = inv;
+            sellInv.left = buyInv;
+            buyInv.right = sellInv;
+        }
+
+        public override void KeyPressed(VirtualKeys key)
+        {
+            if (key == VirtualKeys.Escape)
+            {
+                TradeMessage.SendBreak();
+                TradeCancelled();
+            }
+            else if (key == VirtualKeys.Control)
+            {
+                if (inv.Enabled)
+                {
+                    TradeMessage.OfferItem(inv.selectedItem);
+                }
+                else if (sellInv.Enabled)
+                {
+                    TradeMessage.RemoveItem(sellInv.selectedItem);
+                }
+            }
+            else if(key == AcceptTradeKey)
+            {
+                if (!confirmed)
+                {
+                    confirmed = true;
+                    TradeMessage.ConfirmOffer();
+                    sellInv.SetAcceptStateColor(true);
+                }
+                else
+                {
+                    TradeMessage.DeclineOffer();
+                    DeclineOffer();
+                }
+            }
+            else
+            {
+                if (inv.Enabled)
+                {
+                    inv.KeyPressed(key);
+                    return;
+                }
+                else if (sellInv.Enabled)
+                {
+                    sellInv.KeyPressed(key);
+                    return;
+                }
+                else if (buyInv.Enabled)
+                {
+                    buyInv.KeyPressed(key);
+                    return;
+                }
+            }
+        }
+
+        public override void Open()
+        {
+            itemsInv = new Dictionary<uint, Item>(Player.Inventory);
+
+            buyInv.Show();
+            sellInv.Show();
+            inv.Show();
+            inv.SetContents(Player.Inventory);
+            inv.Enabled = true;
+            base.Open();
+        }
+
+        public override void Close()
+        {
+            buyInv.Hide();
+            sellInv.Hide();
+            inv.Hide();
+            base.Close();
+        }
+
+        public void DeclineOffer()
+        {
+            confirmed = false;
+            sellInv.SetAcceptStateColor(false);
+        }
+
+        public void OtherConfirmedOffer()
+        {
+            otherConfirmed = true;
+            buyInv.SetAcceptStateColor(true);
+        }
+
+        public void OtherDeclinedOffer()
+        {
+            otherConfirmed = false;
+            buyInv.SetAcceptStateColor(false);
+        }
+
+        public void TradeRequested(NPC trader)
+        {
+            // eine anfrage zum Handel hat stattgefunden
+        }
+
+        public void TradeAccepted(NPC trader)
+        {
+            // init any suff that the trade has been accepted
+            Open();
+            sellInv.LeftInfoBox = Player.Hero.Name;
+            buyInv.LeftInfoBox = trader.Name;
+        }
+
+        public void TradeCancelled()
+        {
+            // trader cancelled trading
+            itemsBuy.Clear();
+            buyInv.SetContents(itemsBuy);
+            itemsSell.Clear();
+            sellInv.SetContents(itemsSell);
+            Close();
+        }
+
+        public void ChangeItem(Item item, bool self, bool add)
+        {
+            if(confirmed)
+            {
+                confirmed = false;
+                sellInv.SetAcceptStateColor(false);
+            }
+
+            if(otherConfirmed)
+            { 
+                OtherDeclinedOffer();
+            }
+
+            if (self)
+            {
+                if (add)
+                {
+                    itemsSell.Add(item.ID, item);
+                    itemsInv.Remove(item.ID);
+                }
+                else
+                {
+                    itemsInv.Add(item.ID, item);
+                    itemsSell.Remove(item.ID);
+                }
+                inv.SetContents(itemsInv);
+                sellInv.SetContents(itemsSell);
+            }
+            else
+            {
+                if (add)
+                {
+                    itemsBuy.Add(item.ID, item);
+                }
+                else
+                {
+                    itemsBuy.Remove(item.ID);
+                }
+                buyInv.SetContents(itemsBuy);
+            }
+        }
+    }
+}
---
+++ GMP/Network/Messages/TradeMessage.cs
@@ -0,0 +1,155 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using WinApi;
+using WinApi.User.Enumeration;
+using GUC.Types;
+using GUC.Enumeration;
+using GUC.Client.WorldObjects;
+using GUC.Network;
+using GUC.Client.GUI;
+using RakNet;
+using Gothic.mClasses;
+using Gothic.zClasses;
+
+using GUC.Client.Menus;
+
+namespace GUC.Client.Network.Messages
+{
+    static class TradeMessage
+    {
+
+        public static void Read(BitStream stream)
+        {
+            // call accept - break - change item
+
+            TradeStatus tradeState = (TradeStatus)stream.mReadByte();
+            TradeMenu trade = TradeMenu.GetTrade();
+
+            Item item = null;
+            NPC npc = null;
+
+            if (tradeState >= TradeStatus.SelfOfferItem)
+            {
+                zERROR.GetZErr(Program.Process).Report(2, 'G', "received offer" , 0, "GameState.cs", 0);
+                uint   id          = stream.mReadUInt();
+                ushort instanceId  = stream.mReadUShort();
+                ushort amount      = stream.mReadUShort();
+                ushort condition   = stream.mReadUShort();
+                string specialLine = stream.mReadString();
+
+                if (!World.itemDict.TryGetValue(id, out item))
+                {
+                    // item not existing yet -> creating item
+                    Item newItem = new Item(id, instanceId);
+                    newItem.Amount = amount;
+                    newItem.Condition = condition;
+                    newItem.specialLine = specialLine;
+                    item = newItem;
+                    World.itemDict[id] = newItem;
+                }
+                else
+                {
+                    item = World.itemDict[id];
+                }
+            }
+            else
+            {
+                uint playerID = stream.mReadUInt();
+                World.npcDict.TryGetValue(playerID, out npc);
+            }
+
+            switch (tradeState)
+            {
+                case TradeStatus.Request:
+                    if (npc == null) return;
+                    zERROR.GetZErr(Program.Process).Report(2, 'G', "NPC request by " + npc.ID.ToString(), 0, "GameState.cs", 0);
+                    trade.TradeRequested(npc);
+                    break;
+                case TradeStatus.Accept:
+                    if (npc == null) return;
+                    trade.TradeAccepted(npc);
+                    break;
+                case TradeStatus.Break:
+                    trade.TradeCancelled();
+                    break;
+                case TradeStatus.ConfirmOffer:
+                    trade.OtherConfirmedOffer();
+                    break;
+                case TradeStatus.DeclineOffer:
+                    trade.OtherDeclinedOffer();
+                    break;
+                case TradeStatus.SelfOfferItem:
+                    trade.ChangeItem(item, true, true);
+                    break;
+                case TradeStatus.SelfRemoveItem:
+                    trade.ChangeItem(item, true, false);
+                    break;
+                case TradeStatus.OtherOfferItem:
+                    trade.ChangeItem(item, false, true);
+                    break;
+                case TradeStatus.OtherRemoveItem:
+                    trade.ChangeItem(item, false, false);
+                    break;
+            }
+        }
+
+        public static void ConfirmOffer()
+        {
+            BitStream stream = Program.client.SetupSendStream(NetworkID.TradeMessage);
+            stream.mWrite((byte)TradeStatus.ConfirmOffer);
+            Program.client.SendStream(stream, PacketPriority.HIGH_PRIORITY, PacketReliability.UNRELIABLE);
+        }
+
+        public static void DeclineOffer()
+        {
+            BitStream stream = Program.client.SetupSendStream(NetworkID.TradeMessage);
+            stream.mWrite((byte)TradeStatus.DeclineOffer);
+            Program.client.SendStream(stream, PacketPriority.HIGH_PRIORITY, PacketReliability.UNRELIABLE);
+        }
+
+        public static void RemoveItem(Item item)
+        {
+            if (item == null)
+                return;
+            BitStream stream = Program.client.SetupSendStream(NetworkID.TradeMessage);
+            stream.mWrite((byte)TradeStatus.SelfRemoveItem);
+            stream.mWrite(item.ID);
+            Program.client.SendStream(stream, PacketPriority.HIGH_PRIORITY, PacketReliability.UNRELIABLE);
+            TradeMenu.GetTrade().DeclineOffer();
+        }
+
+        public static void OfferItem(Item item)
+        {
+            if (item == null)
+                return;
+            BitStream stream = Program.client.SetupSendStream(NetworkID.TradeMessage);
+            stream.mWrite((byte)TradeStatus.SelfOfferItem);
+            stream.mWrite(item.ID);
+            stream.mWrite(item.instance.ID);
+            stream.mWrite(item.Amount);
+            stream.mWrite(item.Condition);
+            stream.mWrite(item.specialLine);
+            Program.client.SendStream(stream, PacketPriority.HIGH_PRIORITY, PacketReliability.UNRELIABLE);
+            TradeMenu.GetTrade().DeclineOffer();
+        }
+
+        public static void SendRequest(uint npcId)
+        {
+            BitStream stream = Program.client.SetupSendStream(NetworkID.TradeMessage);
+            stream.mWrite((byte)TradeStatus.Request);
+            stream.mWrite(npcId);
+            Program.client.SendStream(stream, PacketPriority.HIGH_PRIORITY, PacketReliability.UNRELIABLE);
+        }
+
+        public static void SendBreak()
+        {
+            BitStream stream = Program.client.SetupSendStream(NetworkID.TradeMessage);
+            stream.mWrite((byte)TradeStatus.Break);
+            Program.client.SendStream(stream, PacketPriority.HIGH_PRIORITY, PacketReliability.UNRELIABLE);
+        }
+
+    }
+}
