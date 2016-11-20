34adbf565dd64a885f39bb387e139460c0304d15
Florian R. Klein <torpedoente@googlemail.com>
2016-11-20 18:42:36 +0100

    more EffectHandler classs without starting funktionality

---
+++ ScriptsServer/Sumpfkraut/EffectSystem/EffectHandlers/ItemInstEffectHandler.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Reflection;
+
+namespace GUC.Scripts.Sumpfkraut.EffectSystem.EffectHandlers
+{
+
+    public class ItemInstEffectHandler : ItemDefEffectHandler
+    {
+
+        new public static readonly string _staticName = "ItemInstEffectHandler (static)";
+
+
+
+        static ItemInstEffectHandler ()
+        {
+            PrintStatic(typeof(SomeEffectHandler), "Start subscribing listeners to events...");
+            // to do
+            PrintStatic(typeof(SomeEffectHandler), "Finished subscribing listeners to events...");
+        }
+
+        public ItemInstEffectHandler (List<Effect> effects)
+            : this("ItemInstEffectHandler (default)", effects)
+        { }
+
+        public ItemInstEffectHandler (string objName, List<Effect> effects) 
+            : base(objName, effects)
+        { }
+
+
+
+        protected override void ApplyEffectInner (Effect effect, bool reverse = false)
+        {
+            Print("Apply what? Naaaa!");
+        }
+
+    }
+
+}
---
+++ ScriptsServer/Sumpfkraut/EffectSystem/EffectHandlers/VobEffectHandler.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Reflection;
+
+namespace GUC.Scripts.Sumpfkraut.EffectSystem.EffectHandlers
+{
+
+    public class VobEffectHandler : BaseEffectHandler
+    {
+
+        new public static readonly string _staticName = "VobEffectHandler (static)";
+
+
+
+        static VobEffectHandler ()
+        {
+            PrintStatic(typeof(SomeEffectHandler), "Start subscribing listeners to events...");
+            // to do
+            PrintStatic(typeof(SomeEffectHandler), "Finished subscribing listeners to events...");
+        }
+
+        public VobEffectHandler (List<Effect> effects)
+            : this("VobEffectHandler (default)", effects)
+        { }
+
+        public VobEffectHandler (string objName, List<Effect> effects) 
+            : base(objName, effects)
+        { }
+
+
+
+        protected override void ApplyEffectInner (Effect effect, bool reverse = false)
+        {
+            Print("Apply what? Naaaa!");
+        }
+
+    }
+
+}
