bda08f04b908fbb0b7a571b1f9e5eb2452a9ffff
Florian R. Klein <torpedoente@googlemail.com>
2017-01-27 09:30:38 +0100

    trying to clean up the mess, Visual Studio simpyl had to do after a bluescreen... recovering my files my ass

---
+++ ScriptsServer/Sumpfkraut/EffectSystem/Changes/Change.cs
@@ -0,0 +1,19 @@
+﻿using GUC.Scripts.Sumpfkraut.EffectSystem.EffectHandlers;
+using GUC.Scripts.Sumpfkraut.EffectSystem.Enumeration;
+using GUC.Utilities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace GUC.Scripts.Sumpfkraut.EffectSystem.Changes
+{
+
+    public class Change : ExtendedObject
+    {
+
+
+
+    }
+
+}
---
+++ ScriptsServer/Sumpfkraut/EffectSystem/Changes/Change_World_Time.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using GUC.Scripts.Sumpfkraut.EffectSystem.Enumeration;
+using GUC.Scripts.Sumpfkraut.EffectSystem.EffectHandlers;
+
+namespace GUC.Scripts.Sumpfkraut.EffectSystem.Changes
+{
+
+    public class Change_World_Time : BaseChange
+    {
+
+        new public static readonly string _staticName = "Change_Effect_Name (static)";
+
+        new public static readonly ChangeType[] supportedChangeTypes = new ChangeType[]
+        {
+            ChangeType.Effect_Name_Set
+        };
+
+        new public static readonly Type[] parameterTypes = new Type[] 
+        {
+            typeof(string)
+        };
+
+        new public static List<ChangeDestination> influencedDestinations = new List<ChangeDestination>();
+
+
+
+        protected Change_World_Time (Effect effect, ChangeType changeType, object[] parameters) 
+            : base(effect, changeType, parameters)
+        {
+            SetObjName("Change_World_Time (default)");
+        }
+
+
+
+        // use this method to create the objects
+        new public static Change_Effect_Name Create (Effect effect, ChangeType changeType, object[] parameters)
+        {
+            if (!CreateCheckBasics(effect, changeType, parameters, parameterTypes)) { return null; }
+            return new Change_Effect_Name(effect, changeType, parameters);
+        }
+
+    }
+
+}
