268997826ca6acd46b20deffcdedcb6f9820a3b9
Florian R. Klein <torpedoente@googlemail.com>
2017-03-13 13:53:07 +0100

    winding number algorthm - added utility function to calculate wining number which is used to determine whether a point lies within a polygon or not - is not yet used but with a later zone system (wanted to get it out of my head) - is untested !!!

---
+++ ScriptsShared/Sumpfkraut/Utilities/Geometry/InGeometry.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GUC.Scripts.Sumpfkraut.Utilities.Geometry
+{
+
+    public static class InGeometry
+    {
+
+        public static int CalcLeftness (Point testPoint, Point lineStartPoint, Point lineEndPoint)
+        {
+            return ( ((lineEndPoint.X - lineStartPoint.X) * (testPoint.Y - lineStartPoint.Y)) 
+                - ((testPoint.X - lineStartPoint.X) * (lineEndPoint.Y - lineStartPoint.Y)) );
+        }
+
+        public static int CalcWindingNumber2D (Point point, List<Point> polygon)
+        {
+            return CalcWindingNumber2D(point, polygon.ToArray());
+        }
+
+        public static int CalcWindingNumber2D (Point point, Point[] polygon)
+        {
+            if (polygon.Length < 1) { return 0; }
+            int wn = 0;
+            int nextIndex;
+            
+            for (int i = 0; i < polygon.Length; i++)
+            {
+                if (i < (polygon.Length - 1)) { nextIndex = i + 1; }
+                else { nextIndex = 0; }
+
+                if (polygon[i].Y <= point.Y)
+                {
+                    if ((polygon[nextIndex].Y > point.Y) 
+                        && (CalcLeftness(point, polygon[i], polygon[nextIndex]) > 0))
+                    {
+                        wn++;
+                    }
+                }
+                else
+                {
+                    if ((polygon[nextIndex].Y <= point.Y) 
+                        && (CalcLeftness(point, polygon[i], polygon[nextIndex]) < 0))
+                    {
+                        wn--;
+                    }
+                }
+            }
+
+            return wn;
+        }
+
+        public static bool IsInPolygon2D (Point point, List<Point> polygon)
+        {
+            return IsInPolygon2D(point, polygon.ToArray());
+        }
+
+        public static bool IsInPolygon2D (Point point, Point[] polygon)
+        {
+            return CalcWindingNumber2D(point, polygon) != 0;
+        }
+
+
+
+    }
+
+}
